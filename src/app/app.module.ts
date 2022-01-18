03afae8ccccf5ce893700a0a4625f548f7eb872c
Dayo Adebanjo <dahyor@outlook.com>
2022-01-18 12:26:03 +0100

    configured environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UploadModule } from '../upload/upload.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UploadModule],
+  imports: [
+    UploadModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    })
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -2,27 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { writeFile } from 'fs/promises';
 
-
 @Injectable()
 export class UploadService {
-  
-  UPLOAD_PATH = '/uploads'
+  UPLOAD_PATH = process.env.UPLOAD_DESTINATION;
 
   generateUUID() {
     return uuidv4();
   }
 
-  generateFilePath(uuid: string){
-    return `${this.UPLOAD_PATH}/${uuid}`
+  generateFilePath(uuid: string) {
+    return `${this.UPLOAD_PATH}/${uuid}`;
   }
 
   async saveFile(uuid: string, fileBuffer: Buffer) {
     await writeFile(this.generateFilePath(uuid), fileBuffer);
-  } 
+  }
 
   async initiateUpload(file: Express.Multer.File) {
     const uuid = this.generateUUID();
-    await this.saveFile(uuid, file.buffer)
+    await this.saveFile(uuid, file.buffer);
     return uuid;
   }
 }
